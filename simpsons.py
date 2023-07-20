46a5576c7a6b410f0507729c070796212a0693ef
tct123 <42028373+tct123@users.noreply.github.com>
2023-07-20 10:16:34 +0200

    .

--- simpsons.py
+++ simpsons.py
@@ -1,5 +1,11 @@
 import pandas
 
-article = "https://de.wikipedia.org/wiki/Die_Simpsons%2FEpisodenliste?wprov=sfla1"
-simpsonsdata = pandas.read_html(article)
-print(simpsonsdata[0])
+
+def simsonstables():
+    article = "https://de.wikipedia.org/wiki/Die_Simpsons%2FEpisodenliste?wprov=sfla1"
+    simpsonsdata = pandas.read_html(article)
+    with open("simpsons.csv","w") as file:
+        file.write(str(simpsonsdata[0]))
+
+
+simsonstables()
