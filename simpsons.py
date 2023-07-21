0897385d850eb4e300d43c1c049b98e1e0ba4907
tct123 <42028373+tct123@users.noreply.github.com>
2023-07-21 19:41:14 +0200

    Update simpsons.py

--- simpsons.py
+++ simpsons.py
@@ -4,8 +4,9 @@ import pandas
 def simsonstables():
     article = "https://de.wikipedia.org/wiki/Die_Simpsons%2FEpisodenliste?wprov=sfla1"
     simpsonsdata = pandas.read_html(article)
-    with open("simpsons.csv","w") as file:
-        file.write(str(simpsonsdata[0]))
+    for x in range(len(simpsonsdata)):
+        with open(f"simpsons{x}.csv","w") as file:
+            file.write(str(simpsonsdata[0]))
 
 
 simsonstables()
