dad61c9909ceade4977953add17464c584ab1d02
tct123 <42028373+tct123@users.noreply.github.com>
2023-07-26 10:18:51 +0200

    Converting to CSV

--- simpsons.py
+++ simpsons.py
@@ -6,8 +6,7 @@ def simsonstables():
     article = "https://de.wikipedia.org/wiki/Die_Simpsons%2FEpisodenliste?wprov=sfla1"
     simpsonsdata = pandas.read_html(article)
     for x in range(len(simpsonsdata)):
-        with open(f"simpsons{x}.csv","w") as file:
-            file.write(str(simpsonsdata[x]))
+        simpsonsdata[x].to_csv(path_or_buf=f"simpsons{x}.csv")
 
 
 simsonstables()
